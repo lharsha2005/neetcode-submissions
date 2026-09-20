62cb0bb9eb28f3200e511b2ae38563eac9cb7669
Lebaka Harsha Vardhan <lharsha2005@gmail.com>
2026-09-21 00:56:57 +0530

    Add: maximum-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-subarray/submission-6.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size()==1)  return nums[0];
+        int ans=nums[0];
+        int dp=nums[0];
+        for(int i=1;i<nums.size();i++){
+            dp=max(dp+nums[i],nums[i]);
+            ans=max(ans,dp);
+            printf("%d\n",ans);
+        }
+        return ans;
+    }
+};
